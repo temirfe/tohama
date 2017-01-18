99cd0ce9630693bc3590e10552c359e038d88f3e
Temir Asus <temirbek@gmail.com>
2017-01-18 22:38:36 +0600

    some tables'

---
+++ console/migrations/m170118_163620_create_worksheet.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170118_163620_create_worksheet extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170118_163620_create_worksheet cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
